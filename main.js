fac7f893a4cf8112b368fd02daa62b756882779a
daggettt <lorettadaggett.r@gmail.com>
2021-01-28 16:47:39 -0500

    Added clear and delete functions. Still need to link operate function to "=" div.

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+const display = document.getElementById("calDisplay");
+const equal = document.getElementById("clear");
+
+const arr = [];
+
+function addtoArray (num) {
+    
+    arr.push(num);
+    display.textContent = arr.join(' ');
+    console.log(arr);
+}
+
 // basic calculator functions:
 // addition
 function add (x, y) {
@@ -6,13 +18,11 @@ function add (x, y) {
     // let sum = [...arg].reduce((a,b) => a + b, 0);
     return sum;
 }
-
 // subtraction
 function subtract (x, y) {
     let sub = x - y;
     return sub;
 }
-
 //multiplication
 function multiply (x, y) {
     let product = x * y;
@@ -20,13 +30,11 @@ function multiply (x, y) {
     // let product = [...arg].reduce((product, x) => product * x, 1);
     return product;
 }
-
 // division
 function divide (x, y) {
     let div = x / y;
     return div;
 }
-
 // operate function which takes two numbers and an operator (above)
 function operate (...arr) {
     let x = arr[0];
@@ -39,9 +47,22 @@ function operate (...arr) {
             return add(x, y);
         
         } else if (arr[1] == '*') {
-            return multiply(x, y)
+            return multiply(x, y);
         
         } else if (arr[1] == '/') {
             return divide(x, y);
         };
+}
+
+//function for removing the last item of the array.
+function removeNum () {
+    arr.pop();
+    return display.textContent = arr.join(' ');
+}
+
+//function for clearing the calculator's display, and for clearing the array.
+function clearDisplay () {
+    let end = arr.length;
+    arr.splice(0, end);
+    return display.textContent = arr;
 }
