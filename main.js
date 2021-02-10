e8cd7d30835c5b55ff111d01b722a65de6a4d8e6
Rebecca Daggett <lorettadaggett.r@gmail.com>
2021-02-09 21:20:48 -0500

    FINALLY! finished all calculator functions, except for deleting single characters from display

--- main.js
+++ main.js
@@ -1,105 +1,124 @@
-const display = document.getElementById("calDisplay");
-const equal = document.getElementById("equal");
-const operator = document.querySelectorAll(".operator");
-const value = document.querySelectorAll(".value");
+const display = document.querySelector('#calDisplay');
+const keys = document.querySelector('#keypad');
+const clear = document.querySelector('#clear');
+const equal = document.querySelector('#equal');
 
-const arr = [];
+let displayarr = [];
 
-function add (x, y) {
-    let sum = x + y;
-    return sum;
-}
-function subtract (x, y) {
-    let sub = x - y;
-    return sub;
-}
-function multiply (x, y) {
-    let product = x * y;
-    return product;
-}
-function divide (x, y) {
-    let div = x / y;
-    return div;
-}
+let firstValue = null;
+let secondValue = '';
+let operator = null;
+let clearscreen = false;
 
-// operate function which takes two numbers and an operator (above)
-
-function operate (arr) {
-    let x = arr[0];
-    let y = arr[2];
+function addNum (num) {
+    if (clearscreen == true) {
+        display.textContent = '';
+        clearscreen = false;
+    }
     
-        if (arr[1] == "-") {
-            arr = subtract(x, y);
-            display.textContent = arr;
+    let displayNum = num;
+    display.textContent += displayNum;
+}
+
+function calculate (displayarr) {
+    let x = displayarr[0];
+    let y = displayarr[2];
+    let result;
         
-        } else if (arr[1] == "+") {
-            arr = add(x, y);
-            display.textContent = arr;
-        
-        } else if (arr[1] == '*') {
-            arr = multiply(x, y);
-            display.textContent = arr;
-        
-        } else if (arr[1] == '/') {
-            arr = divide(x, y);
-            display.textContent = arr;
-        };
- }       
+        if (displayarr[1] === "-") {
+            result = x - y;
+            //display.textContent = result;
+            //console.log(result);
             
+        } else if (displayarr[1] === "+") {
+            result = x + y;
+            //display.textContent = result;
+            //console.log(result);
             
-// function operate (arr) {
-//     let i = 0;
-//     let newarr = arr.slice(i, 3);
-//         let x = newarr[0];
-//         let y = newarr[2];
-                
-//             if (newarr[1] == "-") {
-//                 newarr = subtract(x, y);
-//                 console.log(newarr);
-                    
-//             } else if (newarr[1] == "+") {
-//                 newarr = add(x, y);
-//                 console.log(newarr);
-                    
-//             } else if (newarr[1] == '*') {
-//                 newarr = multiply(x, y);
-//                 console.log(newarr);
-                    
-//             } else if (newarr[1] == '/') {
-//                 newarr = divide(x, y);
-//                 console.log(newarr);
-//                 };
-//         }
-                
-function calculate (arr) {
-    for(i = 0; i < arr.length; i++) {
-        let newarr = arr.slice(i, 3);
-        operate(newarr);
-        
-        
-       console.log(arr.splice(0, 3, newarr));
-        
-    };
-        
-        
+        } else if (displayarr[1] === 'x') {
+            result = x * y;
+            // display.textContent = result;
+            //console.log(result);
+            
+        } else if (displayarr[1] === '/') {
+            result = x / y;
+            // display.textContent = result;
+            //console.log(result);
+        };  
+    
+        return result;
 }
 
+function addOperator (op) {
+    clearscreen=true;
+    
+    if (displayarr.length === 2) {
+        let secondValue = display.textContent;
+        displayarr[2] = parseFloat(secondValue)
+        
+        display.textContent = calculate(displayarr);
+        
+        let firstValue = display.textContent;
+        displayarr.unshift(parseFloat(firstValue));
+        displayarr.splice(1); 
 
-//function that onclick adds value to array.
-function addtoArray (num) {
-    arr.push(num);
-    display.textContent = arr.join(' ');
-    console.log(arr);
+        let operator = op;
+        displayarr[1] = operator;
+        //console.log(displayarr);
+    
+    } else {
+        let operator = op;
+
+        let firstValue = display.textContent;
+        displayarr[0] = parseFloat(firstValue);
+        
+        displayarr[1] = operator;
+        console.log('else');
+    
+        console.log(displayarr);
+    }
 }
 
-//function for removing the last item of the array.
-function removeNum () {
-    arr.pop();
-    return display.textContent = arr.join(' ');
+function addDecimal (dot) {
+    if (!display.textContent.includes(dot)) {
+       display.textContent += dot;
+        
+    } else {
+        alert('ERROR: too many decimals');
+        display.textContent = null;
+    }
 }
-//function for clearing the calculator's display, and for clearing the array.
-function clearDisplay () {
-    let end = arr.length;
-    arr.splice(0, end);
-    return display.textContent = arr;
+
+function cleardisplay () {
+        let end = displayarr.length;
+        displayarr.splice(0, end);
+        return display.textContent = displayarr;
 }
+
+keys.addEventListener('click', (e) => {
+    const click = e.target;
+
+    if (click.classList.contains('value')) {
+        addNum(click.value);
+    
+    } else if (click.classList.contains('decimal')) {
+        addDecimal(click.value);
+    
+    } else if (click.classList.contains('operator')) {
+        addOperator(click.value);
+    
+    } else if (click.classList.contains('equal')) {
+        let secondValue = display.textContent;
+        displayarr[2] = parseFloat(secondValue);
+        display.textContent = calculate(displayarr);
+        
+        clearscreen = true;
+        displayarr.splice(0, displayarr.length);
+
+    } else if (click.classList.contains('clear')) {
+        cleardisplay();
+    
+    } else if (click.classList.contains('delete')) {
+        console.log(click.value);
+    } 
+});
